7bb366bb014959f64f1ac3c82ebdc99523783c89
hitu <hitarthirai14@gmail.com>
2023-03-09 18:09:34 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TeacherDashboardComponent } from './teacher-dashboard/teacher-dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TeacherDashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
